fbb032bbf947b3c1d705bb720ab8a297e9b65952
Ohoud Alawad <33830158+OhoudAlawad@users.noreply.github.com>
2021-04-18 20:43:54 +0300

    Create SuccessLoginReport.sh

---
+++ SuccessLoginReport.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "
+                 Welcome to Login Attempts Report
+
+                           Programing By
+
+                           Ohoud Alawad
+
+"
+echo " Num of Logins      Date       User         IP"
+echo "---------------------------------------------------------------"
+grep "Accepted password" /var/log/auth.log | awk '{ print "   \t    "$1" "$2"\t"$9"\t  "$11}' | sed s/rhost=// | sort | uniq -c
+echo " "
+echo "---------------------------------------------------------------"
